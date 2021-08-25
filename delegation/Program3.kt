bcd95c7677c5e0059374ceb8af1d5c08bcad1e2c
sanjaydraws <sprajapat8331@gmail.com>
2021-08-25 23:45:41 +0530

    standard delegates

--- delegation/Program3.kt
+++ delegation/Program3.kt
@@ -1,11 +1,36 @@
 // standard delegates 
+// lazy property - it used to create an object that is initialized only first access  to the property value and store vaue and return 
+
+// lazy is just a function function 
+// public fun <T> lazy(mode: LazyThreadSafetyMode, initializer: () -> T): Lazy<T> =
+//         when (mode) {
+//             LazyThreadSafetyMode.SYNCHRONIZED -> SynchronizedLazyImpl(initializer)
+//             LazyThreadSafetyMode.PUBLICATION -> SafePublicationLazyImpl(initializer)
+//             LazyThreadSafetyMode.NONE -> UnsafeLazyImpl(initializer)
+//         }
+
+// by default lazy is synchronized the value is computed only in one thread ,and value will be same in all thread 
+// LazyThreadSafetyMode.PUBLICATION  multiple thread can execute simutanesly 
+// LazyThreadSafetyMode.None  -> unsafe thread 
+
 
 val ourValue:String by lazy{
-    println("Hello World")
-    "Hello what is going on "
+    println("Hello World") 
+    callAnother() // only first time run
+    "\nHello what is going on "
+}
+
+ fun callAnother(){
+    print("another fun ")
 }
 
  fun main(args: Array<String>) {
-     println(ourValue)
+     println(ourValue) //Hello what is going on
+     println(ourValue) //Hello what is going on
+     println(ourValue) //Hello what is going on
+
+
+
 }
+
 
