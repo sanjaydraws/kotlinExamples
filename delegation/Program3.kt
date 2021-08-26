2adf5a896fc2f0bf2fb5b1a0bb4f20bbfb9349e7
sanjaydraws <sprajapat8331@gmail.com>
2021-08-27 00:48:23 +0530

    delegates

--- delegation/Program3.kt
+++ delegation/Program3.kt
@@ -14,7 +14,7 @@
 // LazyThreadSafetyMode.None  -> unsafe thread 
 
 
-val ourValue:String by lazy(LazyThreadSafetyMode.NONE){
+val ourValue:String by lazy(){
     println("Hello World") 
     callAnother() // only first time run
     "\nHello what is going on "
@@ -28,6 +28,7 @@ val ourValue:String by lazy(LazyThreadSafetyMode.NONE){
      println(ourValue) //Hello what is going on
      println(ourValue) //Hello what is going on
      println(ourValue) //Hello what is going on
+    //  ourValue = 90
     
 }
 
--- delegation/Program4.kt
+++ delegation/Program4.kt
@@ -1,9 +1,10 @@
 // Observable Properties 
+//lambda will be triggerd every time when value of property will be changed. 
 import kotlin.properties.Delegates
 class Personn{
     var ourValue: String by Delegates.observable("Hello World"){ property ,oldValue,newValue ->
          print("\n prop $property ")
-         print("\n oldValue $oldValue")
+         print("\n oldValue $oldValue ") 
          print("\n newValue $newValue ")
     }
 }
@@ -13,3 +14,9 @@ fun main(args: Array<String>) {
     p.ourValue = "change world"
 
 }
+
+// output
+// Hello World
+//  prop property ourValue (Kotlin reflection is not available)
+//  oldValue Hello World
+//  newValue change world
