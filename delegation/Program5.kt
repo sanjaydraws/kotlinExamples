e802ffd459ade32db93352e79b877fa45097f32f
sanjaydraws <sprajapat8331@gmail.com>
2021-08-28 00:19:03 +0530

    not null delegate

--- delegation/Program5.kt
+++ delegation/Program5.kt
@@ -1,13 +1,19 @@
 // vetoable - 
+// notnull -- this property allows to initialize later 
+
 
 import kotlin.properties.Delegates
 
 class Person1{
+
+
     var ourValue by Delegates.vetoable("Hello") {
         property, oldValue, newValue -> 
         newValue.length > oldValue.length  // condition should be true to assign new value 
     }
 }
+var fullname: String by Delegates.notNull<String>()
+
 
 
 fun main(args: Array<String>) {
@@ -15,6 +21,12 @@ fun main(args: Array<String>) {
     p1.ourValue = "World"  // Hello
     p1.ourValue = "Worlds"
     println(p1.ourValue) // Worlds 
+
+    // can't access without initialization , gives exception 
+    print(fullname) //  java.lang.IllegalStateException
+    fullname = "Justin Bieber"
+    print(fullname) // Justin Bieber
+
 
 
 }
