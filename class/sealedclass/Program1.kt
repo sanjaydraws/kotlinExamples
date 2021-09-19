534313658346addc12bfeb953442a9c006a16cce
sanjaydraws <sprajapat8331@gmail.com>
2021-09-19 23:55:41 +0530

    sealed class

--- class/sealedclass/Program1.kt
+++ class/sealedclass/Program1.kt
@@ -1,22 +1,36 @@
 
 data class User(val firstName:String? = null , val age:Int? = null )
 
-/**
- * @a
- */
+
 
 sealed class Resource<T>(
     val data:T? = null ,
     val message:String? = null 
 ){
-    class Success<T>(data:T? = null ):Resource<T>(data) 
+     class Success<T>( data:T? = null, message:String? = null ):Resource<T>(data,message) 
     class Loading<T>(data:T? = null ):Resource<T>(data)
     class Error<T>(message:String,data:T? = null ):Resource<T>(data, message)
+}
+
+fun getUserData():Resource<User>{
+    return  Resource.Success(User("Justin",45), message ="Suceess")
 }
 
 
 fun main(args: Array<String>) {
 
-    
+    val r =getUserData()
+    print(r.data)
+    print(r.message) 
+
+    when(r.message){
+       "Suceess" ->{
+            print("Success")
+        }
+
+
+    }
+
+
 }
 
