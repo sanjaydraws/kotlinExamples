9152263f6117bc501a405a8ee04febfa18a233bd
sanjaydraws <sprajapat8331@gmail.com>
2021-09-21 22:39:51 +0530

    scope builder

---
+++ coroutine/Program1.kt
@@ -0,0 +1,77 @@
+// A coroutine is an instance of suspendable computation. 
+// It may suspend its execution in one thread and resume in another one.
+//Coroutines can be thought of as light-weight threads
+
+// example 1
+fun main() = runBlocking{
+    
+    println(currentCoroutineContext())
+    launch{
+        // launch a new coroutine and continoue 
+        delay(100L)
+        println("Hello World1 ")
+        println(currentCoroutineContext())
+    }
+    launch{
+        // launch a new coroutine and continoue 
+        delay(100L)
+        println("Hello World2 ")
+        println(currentCoroutineContext())
+    }
+    println("Hello world3")
+}
+
+// launch -> coroutine Builder , it launches new coroutine concurrently , with rest of code 
+// delay() - it suspends the coroutine for specific time , it does not block the underlying thread, but allows other coroutine to run and use the underlying thread for their code 
+
+// runBlocking is also coroutine builder , it is bridge b/q non coroutine world of regular main and and code with coroutine inside runblocking 
+//The name of runBlocking means that the thread that runs it (in this case â€” the main thread) gets blocked for the
+// duration of the call, until all the coroutines inside runBlocking { ... } complete their execution
+
+
+// Structured concurrency 
+// Coroutines follow a principle of structured concurrency which means that new coroutines can be only launched in a
+// specific CoroutineScope which delimits the lifetime of the coroutine
+
+// suspend function 
+
+fun main() = runBlocking{
+    launch{
+        print(doWorld())
+    }
+    println("Hello")
+}
+
+// suspend function
+suspend fun doWorld():Int {
+    delay(1000L)
+    println("World!")
+    return 0
+}
+
+
+// ==================================
+// scope builder 
+// can create your own scope using the coroutine scope builder 
+// runBlocking and coroutineScope builders may look similar because they both wait for their body and all its children to
+// complete
+// runblocking - blocks the current thread for waiting , that's why it's regular function 
+// coroutineScope - just suspends,releasing the underlying thread for other usages
+
+
+
+fun main()= runBlocking{
+    println(currentCoroutineContext())//BlockingCoroutine
+    myWorld()
+}
+suspend fun myWorld() = coroutineScope{
+    println(currentCoroutineContext())//ScopeCoroutine
+    launch{
+        delay(1000L)
+        println("World")
+        println(currentCoroutineContext()) // StandaloneCoroutine
+    }
+    println("World2")
+}
+
+
--- coroutine/asynchronousFlow/Program2.kt
+++ coroutine/asynchronousFlow/Program2.kt
@@ -1,15 +1,26 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 
-fun simple(): Flow<Int> = flow { // flow builder
-    for (i in 1..3) {
-    delay(100) // pretend we are doing something useful here
-    emit(i) // emit next value
+fun simple():Flow<Int> = flow{
+    // flow builder 
+    for(i in 1..3){
+        delay(100) // pretending we are doing something usefull here 
+        emit(1)
     }
-    }
-    fun main() = runBlocking<Unit> {
+}
+
+fun main() = runBlocking<Unit>{
     // Launch a concurrent coroutine to check if the main thread is blocked
-    launch {
-    for (k in 1..3) {
-    println("I'm not blocked $k")
-    delay(100)
+    launch{
+        for(k in 1..3){
+            println("I am not blocked $k")
+            delay(100)
+        }
     }
+    
+    // collect the flow 
+    simple().collect{
+        value -> println(value)
+    }
+}
