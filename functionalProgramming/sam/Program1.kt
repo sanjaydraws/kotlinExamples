9bff7fecb7ff0edb13d38c4fdb89782c997a9dcd
sanjaydraws <sprajapat8331@gmail.com>
2021-11-21 12:11:11 +0530

    sam interface

--- functionalProgramming/sam/Program1.kt
+++ functionalProgramming/sam/Program1.kt
@@ -14,6 +14,14 @@ fun interface FunctInterface{
     fun invoke(list: List<Int>) :List<Int>
 }
 
+// functional interface
+interface FuncInterface3 {
+    fun abstract_method(a: Int, b: String?)
+    // Second method prevents lambda:
+    fun   nonabstract_method() {
+        println("do something")
+    }
+}
 
 fun main(){
 
@@ -39,6 +47,24 @@ fun main(){
         it.filter { pre -> pre %2 == 0 }
     }
     print(fi2.invoke(listOf(2,5,1,5,79,0))) // [2, 0]
+
+
+    val fi3 = object : FuncInterface3{
+        override fun abstract_method(a: Int, b: String?) {
+            print("This is abstract method")
+        }
+
+        // it's not force to override this method
+        override fun nonabstract_method() {
+            print("overriding non abstract method")
+        }
+    }
+
+    fi3.nonabstract_method() // do something
+    fi3.abstract_method(2,"323") //overriding non-abstract method
+
+
+
 
 
 
