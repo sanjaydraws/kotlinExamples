12e29d02420aee12c22624686beadd30bb42f1a2
sanjaydraws <sprajapat8331@gmail.com>
2021-11-27 22:49:49 +0530

    sealed class

--- class/sealedclass/Program2.kt
+++ class/sealedclass/Program2.kt
@@ -1,6 +1,7 @@
 
 sealed interface Example1{
     fun open1(param1 :Int)
+
 }
 
 sealed interface Example2{
@@ -17,13 +18,44 @@ object O1:Example1,Example2{
     }
 }
 
+data class Sum(val x :Int?, val y:Int?):Example1 {
+    override fun open1(param1: Int) {
+        print(y?.let { x?.plus(it) } ?: 0)
+    }
+}
+
+// constructor of sealed class has two visibility protected(by default) or private
+// public and internal not allowed
 sealed class M1 :Example1 {
+
+    constructor(){
+        // protected
+        println("by default it's protected")
+    }
+    private constructor(x: Int?):this(){
+        println(this)
+    }
     fun m(){
         println("this is f1 ")
     }
 }
+sealed class M2(val x:Int):Example1{
+    // not necessary to override
+}
 
-fun main(args: Array<String>) {
+// inherit sealed class
+data class Minus(val x :Int?, val y :Int?):M1(x){
+    init {
+        println("x=$x  y=$y")
+    }
+    override fun open1(param1: Int) {
+
+    }
+}
+
+
+fun main() {
+
 
     //    This type is sealed, so it can be inherited by only its own nested classes or objects
     /*
@@ -34,8 +66,21 @@ fun main(args: Array<String>) {
     }     */
 
     O1.open1(3)
+    val s =  Sum(2,3)
+    s.open1(1) // 5
 
 
-    
+    val min = Minus(23,6)
+    /**
+    by default it's protected
+    Minus(x=null, y=null)
+    x=23  y=6
+
+     * */
+    // can't instantiate sealed class directly 
+
+
+
+
+
 }
-
